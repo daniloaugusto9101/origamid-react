58a423ec90ba5fb5d365c6d132f4385800855f46
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-09-26 21:01:46 -0300

    feat: Criar desafio 2 da aula inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,42 @@
 import React from "react";
 
-const luana = {
-  cliente: "Luana",
-  idade: 27,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-  ],
-  ativa: true,
-};
-
-const mario = {
-  cliente: "Mario",
-  idade: 31,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-    { nome: "Guitarra", preco: "R$ 3500" },
-  ],
-  ativa: false,
-};
+const produtos = [
+  {
+    id: 1,
+    nome: "Smartphone",
+    preco: "R$ 2000",
+    cores: ["#29d8d5", "#252a34", "#fc3766"],
+  },
+  {
+    id: 2,
+    nome: "Notebook",
+    preco: "R$ 3000",
+    cores: ["#ffd045", "#d4394b", "#f37c59"],
+  },
+  {
+    id: 3,
+    nome: "Tablet",
+    preco: "R$ 1500",
+    cores: ["#365069", "#47c1c8", "#f95786"],
+  },
+];
 
 const App = () => {
-  const dados = luana;
-  const isActive = dados.ativa ? "Ativa" : "Invativa";
-  const classActive = dados.ativa ? "text-green-700" : "text-red-700";
-  const totalGastos = dados.compras.reduce((acc, item) => {
-    return acc + Number(item.preco.replace("R$ ", ""));
-  }, 0);
-
   return (
-    <div className="text-4xl">
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>
-        Situação: <span className={`${classActive}`}>{isActive}</span>{" "}
-      </p>
-      <p>Total de gasto: R$ {totalGastos}</p>
-      {totalGastos > 10000 && <small>Você gastou mais que o planejado</small>}
+    <div>
+      {produtos.map(({ id, nome, preco, cores }) => (
+        <div key={id} className="mb-6">
+          <p>{nome}</p>
+          <p>Preço: {preco}</p>
+          <ul className="ml-4">
+            {cores.map((cor) => (
+              <li key={cor} style={{ background: cor }}>
+                {cor}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
