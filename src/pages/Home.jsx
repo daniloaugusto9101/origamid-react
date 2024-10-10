d0d8bcec1e9d9daa55403c54ad126b8a600b0e95
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-10-09 22:00:43 -0300

    feat: Criar um botao com estado ativo e inativo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [ativo, setAtivo] = React.useState(false);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <button
+        className={`${ativo ? "bg-slate-500" : "bg-red-800"} p-4`}
+        onClick={() => setAtivo(!ativo)}
+      >
+        clique
+      </button>
+    </div>
+  );
 };
 
 export default Home;
