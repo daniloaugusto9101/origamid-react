5bebfddce96b065ca908544b3eb480332efee5ab
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-10-14 06:42:13 -0300

    feat: Criar um botão com contador

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,21 @@ import React from "react";
 
 const Home = () => {
   const [ativo, setAtivo] = React.useState(false);
+  const [dados, setDados] = React.useState(0);
+
+  function handleClick() {
+    setAtivo(!ativo);
+    setDados(dados + 1);
+  }
 
   return (
     <div>
       <h1>Home</h1>
       <button
         className={`${ativo ? "bg-slate-500" : "bg-red-800"} p-4`}
-        onClick={() => setAtivo(!ativo)}
+        onClick={handleClick}
       >
-        clique
+        {dados}
       </button>
     </div>
   );
