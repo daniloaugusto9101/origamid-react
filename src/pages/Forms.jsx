19fcd863cc13bc8d36e2746b41a22309b2ec24d8
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-12-05 08:16:02 -0300

    feat: Implement form submission handling

--- src/pages/Forms.jsx
+++ src/pages/Forms.jsx
@@ -3,11 +3,18 @@ import React from "react";
 const Forms = () => {
   const [name, setName] = React.useState("");
   const [email, setemail] = React.useState("");
+
+  function handleDubmit(event) {
+    event.preventDefault();
+    //faça alguma coias
+    console.log("cheogu no form");
+  }
   return (
-    <form>
+    <form onSubmit={handleDubmit}>
       <input type="text" className="border" value={name} onChange={(event) => setName(event.target.value)} />
 
-      <input type="email" className="border" value={email} onChange={(event) => setName(event.target.value)} />
+      <input type="email" className="border" value={email} onChange={(event) => setemail(event.target.value)} />
+      <button>Enviar</button>
     </form>
   );
 };
