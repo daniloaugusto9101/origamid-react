b4a681059fee015b901736fcbf3441e270a2b975
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-12-26 07:40:05 -0300

    feat: Componentização do select0

--- src/components/Form/FormCompras.jsx
+++ src/components/Form/FormCompras.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Input from "./Input";
+import Select from "./Select";
 
 const FormCompras = () => {
   const [nome, setNome] = React.useState("");
   const [email, setEmail] = React.useState("");
+  const [produto, setProduto] = React.useState("");
   return (
     <form>
       <Input id="nome" label="Nome" value={nome} setValue={setNome} />
       <Input id="email" label="Email" value={email} setValue={setEmail} />
+      <Select options={["smartphone", "computador"]} value={produto} setValue={setProduto} />
     </form>
   );
 };
