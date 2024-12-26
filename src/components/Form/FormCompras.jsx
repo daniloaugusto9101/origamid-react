3fa7a6f40a6332edcd19e605f477a88b9a5ee49a
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-12-26 08:25:34 -0300

    feat: Componentização do checkbox

--- src/components/Form/FormCompras.jsx
+++ src/components/Form/FormCompras.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import Input from "./Input";
 import Select from "./Select";
+import Radio from "./Radio";
+import Checkbox from "./Checkbox";
 
 const FormCompras = () => {
   const [nome, setNome] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [produto, setProduto] = React.useState("");
+  const [cores, setCores] = React.useState("");
+  const [fruta, setFruta] = React.useState([]);
   return (
     <form>
       <Input id="nome" label="Nome" value={nome} setValue={setNome} />
       <Input id="email" label="Email" value={email} setValue={setEmail} />
       <Select options={["smartphone", "computador"]} value={produto} setValue={setProduto} />
+      <Radio options={["Azul", "Vermelho", "Preto"]} value={cores} setValue={setCores} />
+      <Checkbox options={["Uva", "Laranja", "Limão"]} value={fruta} setValue={setFruta} />
     </form>
   );
 };
---
+++ src/components/Form/Radio.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Radio = ({ options, value, setValue, ...props }) => {
+  return (
+    <>
+      {options.map((option) => (
+        <div key={option}>
+          <label htmlFor="">{option}</label>
+          <input type="radio" value={option} checked={value === option} onChange={({ target }) => setValue(target.value)} {...props} />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Radio;
