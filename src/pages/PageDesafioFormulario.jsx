48557358bfb84f1fbf2942971a4e21de7310ea1e
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-02-03 08:12:15 -0300

    feat: Desafio questionario

--- src/pages/PageDesafioFormulario.jsx
+++ src/pages/PageDesafioFormulario.jsx
@@ -29,7 +29,8 @@ const perguntas = [
 
 const PageDesafioFormulario = () => {
   const [radio, setRadio] = React.useState({}); // Armazena a seleção de cada pergunta
-  console.log(radio);
+  const [proxPergunta, setProxPergunta] = React.useState(0); // Armazena o estado da proxima pergunta
+  const [acertos, setAcertos] = React.useState(0); // Armazena a quantidade de acertos
 
   const onRadio = (idPergunta, respostaSelecionada) => {
     setRadio((prev) => ({
@@ -38,23 +39,53 @@ const PageDesafioFormulario = () => {
     }));
   };
 
-  return (
-    <div className="flex flex-col gap-4 p-10">
-      {perguntas.map((questao) => (
-        <div key={questao.id}>
-          <p>{questao.pergunta}</p>
-          {questao.options.map((resp, index) => (
-            <div key={index}>
-              <input type="radio" name={questao.id} value={resp} id={`${questao.id}-resp${index}`} onChange={() => onRadio(questao.id, resp)} checked={radio[questao.id] === resp} />
-              <label htmlFor={`${questao.id}-resp${index}`}>{resp}</label>
-            </div>
-          ))}
-        </div>
-      ))}
+  const handleNext = () => {
+    totalAcertos(radio[perguntas[proxPergunta].id]);
+    if (proxPergunta < perguntas.length) setProxPergunta((prev) => prev + 1);
+  };
 
-      {/* Exibe as respostas selecionadas */}
-      <h3>Respostas Selecionadas:</h3>
-      <pre>{JSON.stringify(radio, null, 2)}</pre>
+  const totalAcertos = (respSelecionada) => {
+    if (respSelecionada == perguntas[proxPergunta].resposta) {
+      setAcertos((prev) => prev + 1);
+    }
+    console.log("acertos", acertos);
+  };
+
+  const handlePrev = () => {
+    if (proxPergunta > 0) setProxPergunta((prev) => prev - 1);
+  };
+
+  return (
+    <div>
+      {proxPergunta >= perguntas.length ? (
+        <h1>Parabéns, você acertou {acertos} perguntas</h1>
+      ) : (
+        <>
+          <div className="flex flex-col gap-4 p-10">
+            <p>
+              Nº {proxPergunta + 1} - {perguntas[0].pergunta}
+            </p>
+            {perguntas[proxPergunta].options.map((resp, index) => (
+              <div key={index}>
+                <input type="radio" name={perguntas[proxPergunta].id} value={resp} id={`${perguntas[proxPergunta].id}-resp${index}`} onChange={() => onRadio(perguntas[proxPergunta].id, resp)} checked={radio[perguntas[proxPergunta].id] === resp} />
+                <label htmlFor={`${perguntas[proxPergunta].id}-resp${index}`}>{resp}</label>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center gap-5">
+            <button className="bg-blue-400 p-2" onClick={handlePrev}>
+              Anterior
+            </button>
+            <button className="bg-blue-400 p-2" onClick={handleNext}>
+              Próximo
+            </button>
+          </div>
+
+          {/* Exibe as respostas selecionadas */}
+          <h3>Respostas Selecionadas:</h3>
+          <pre>{JSON.stringify(radio, null, 2)}</pre>
+        </>
+      )}
     </div>
   );
 };
